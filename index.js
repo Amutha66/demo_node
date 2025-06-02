4376142feb238f220664932b4070551389d34202
Amutha66 <23026041@myrp.edu.sg>
2025-06-02 13:32:23 +0800

    Initial commit: Basic Node.js project setup

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * This is a demo Node.js application
+ * @author Your Name
+ * @date June 2, 2025
+ */
+
+/**
+ * A placeholder function that demonstrates basic functionality
+ * @param {string} name - The name to greet
+ * @returns {string} A greeting message
+ */
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to the demo application.`;
+}
+
+// Example usage
+console.log(greetUser('Developer'));
